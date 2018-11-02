5ab3d064e45aa5104b140f1c4877cbbece5c8225
atweathers <atweath@g.clemson.edu>
2018-11-02 13:43:42 -0400

    Added .csv files, data pre-processing, and EDA R files

---
+++ Src/EDA.R
@@ -0,0 +1,61 @@
+
+rm(list=ls())
+
+library(ggplot2)
+
+# Name(s): Andrew Weathers ...
+# Date: 17 October 2018
+# Purpose: Initial exploration into our pre-processed datasets
+
+# Import pre-processed data
+speed <- read.csv("../Data/Mod_Speed.csv")
+crash <- read.csv("../Data/Mod_Crash.csv")
+
+speed$X <- NULL
+crash$X <- NULL
+
+# Ensure Speed violation camera is perfectly correlated with Latitude and Longitude
+camLocation <- data.frame(speed$CAMERA.ID, speed$LATITUDE, speed$LONGITUDE)
+# --- MISSING: official results of this
+
+# Remove camera from dataset to avoid overfitting
+speed$CAMERA.ID <- NULL
+
+# Round Longitude and Latitude to 2 decimal places
+#speed$LATITUDE <- round(speed$LATITUDE, 2)
+#speed$LONGITUDE <- round(speed$LONGITUDE, 2)
+
+#speed[, lapply(.SD, round, 2), "LONGITUDE"]
+#speed[, lapply(.SD, round, 2), "LATITUDE"]
+
+#crash$LATITUDE <- round(crash$LATITUDE, 2)
+#crash$LONGITUDE <- round(crash$LONGITUDE, 2)
+
+#crash[, lapply(.SD, round, 2), "LONGITUDE"]
+#crash[, lapply(.SD, round, 2), "LATITUDE"]
+
+# Merge datasets by similar features
+
+# Common date 
+crash$date <- substring(crash$CRASH_DATE, 1, 10)
+speed$date <- speed$VIOLATION.DATE
+
+# Define type to differentiate
+crash$type <- "crash"
+speed$type <- "speed"
+
+# Aggregate df's by unique identifiers
+coords <- merge(crash, speed, by=c("date", "LONGITUDE", "LATITUDE"), all=TRUE)
+coords <- coords[, c("date", "type.x", "type.y", "LONGITUDE", "LATITUDE")]
+coords$type <- paste(coords$type.x, coords$type.y)
+
+# Remove NA tags
+coords$type <- gsub("NA", "", as.character(coords$type))
+coords <- coords[,c("date", "type", "LONGITUDE", "LATITUDE")]
+
+# Initial Data Visualization
+p <- ggplot(coords, aes(x=LONGITUDE, y=LATITUDE, col=type)) +
+            geom_point()
+
+
+
---
+++ Src/pre-processing.R
@@ -0,0 +1,41 @@
+
+library(caret)
+rm(list=ls())
+
+# Name(s): Andrew Weathers ...
+# Date: 10 October 2018
+# Purpose: Aggregate traffic violations dataset with crash dataset
+# by common features
+
+# Import datasets
+speed <- read.csv("../Data/Speed_Camera_Violations.csv")
+crash <- read.csv("../Data/Traffic_Crashes_-_Crashes.csv")
+
+# Remove observations which are missing a location 
+speed <- speed[!is.na(speed$LONGITUDE),]
+speed <- speed[!is.na(speed$LATITUDE),]
+
+crash <- crash[!is.na(crash$LONGITUDE),]
+crash <- crash[!is.na(crash$LATITUDE),]
+
+# Remove observations with little variance
+speedRem <- nearZeroVar(speed)
+crashRem <- nearZeroVar(crash)
+
+# Apply NA to all blank entries in our data
+speed[speed == ""] <- NA
+crash[crash == ""] <- NA
+
+if(length(speedRem) > 0)
+  speed <- speed[-speedRem]
+
+if(length(crashRem) > 0)
+  crash <- crash[-crashRem]
+
+# Write both updated speed and crash datasets to file
+write.csv(crash, file="../Data/Mod_Crash.csv")
+write.csv(speed, file="../Data/Mod_Speed.csv")
+
+# Combine datasets by location
+
+
