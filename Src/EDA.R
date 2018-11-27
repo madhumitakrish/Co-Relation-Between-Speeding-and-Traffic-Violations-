47fe5e2f5450aadd9c703930ac2139dee3d43163
atweathers <atweath@g.clemson.edu>
2018-11-27 11:46:19 -0500

    Moved non-visualizations to Pre-Processing.R

--- Src/EDA.R
+++ Src/EDA.R
@@ -9,66 +9,10 @@ library(ggplot2)
 # Import pre-processed data
 speed <- read.csv("../Data/Mod_Speed.csv")
 crash <- read.csv("../Data/Mod_Crash.csv")
-
+coords <- read.csv("../Data/Aggr_Crash_Speed.csv")
 speed$X <- NULL
 crash$X <- NULL
-
-# Ensure Speed violation camera is perfectly correlated with Latitude and Longitude
-allCamViolations <- data.frame(speed$CAMERA.ID, speed$LATITUDE, speed$LONGITUDE)
-# --- MISSING: official results of this
-
-# Remove camera from dataset to avoid overfitting
-speed$CAMERA.ID <- NULL
-
-# Merge datasets by similar features
-
-# Common date 
-crash$date <- substring(crash$CRASH_DATE, 1, 10)
-speed$date <- speed$VIOLATION.DATE
-
-# Define type to differentiate
-crash$type <- "crash"
-speed$type <- "speed"
-
-# Aggregate df's by unique identifiers
-coords <- merge(crash, speed, by=c("date", "LONGITUDE", "LATITUDE"), all=TRUE)
-coords <- coords[, c("date", "type.x", "type.y", "LONGITUDE", "LATITUDE")]
-coords$type <- paste(coords$type.x, coords$type.y)
-
-# Remove NA tags
-coords$type <- gsub("NA", "", as.character(coords$type))
-coords <- coords[,c("date", "type", "LONGITUDE", "LATITUDE")]
+coords$X <- NULL
 
 # Initial Data Visualization
 p <- ggplot(coords, aes(x=LONGITUDE, y=LATITUDE, col=type)) + geom_point()
-
-# Finding Each Camera
-library(sqldf)
-EveryCamera <- sqldf('SELECT DISTINCT * FROM allCamViolations')
-EveryCamera$CameraID <- gsub("CHI", "", as.character(EveryCamera$speed.CAMERA.ID))
-
-# Loop function to assign a nearest speeding camera
-# to every crash based on Euclidean distance. 
-
-this.distance <- 0
-chosen.camera <- NULL
-regions <- rep("blank", nrow(crash))
-cameras <- EveryCamera[,"CameraID"]
-
-crash.long <- crash[,"LONGITUDE"]
-cam.long <- EveryCamera[,"speed.LONGITUDE"]
-cam.lat <- EveryCamera[,"speed.LATITUDE"]
-crash.lat <- crash[,"LATITUDE"]
-for(x in 1:nrow(crash)){
-  shortest.distance <- 10000
-  #Find shortest distance for given crash
-  for(y in 1:nrow(EveryCamera)){
-    # Compute Euclidean distance
-    this.distance <- (crash.long[x] - cam.long[y])^2 + (crash.lat[x] - cam.lat[y])^2
-    if(this.distance < shortest.distance){
-      shortest.distance = this.distance
-      regions[x] <- cameras[y]
-    }
-  }
-}
-
--- Src/pre-processing.R
+++ Src/pre-processing.R
@@ -1,5 +1,4 @@
 
-library(caret)
 rm(list=ls())
 
 # Name(s): Andrew Weathers ...
@@ -11,34 +10,62 @@ rm(list=ls())
 speed <- read.csv("../Data/Speed_Camera_Violations.csv")
 crash <- read.csv("../Data/Traffic_Crashes_-_Crashes.csv")
 
-# Remove observations which are missing a location 
+# Strip crash of uneeded features
+crash <- crash[,c("CRASH_DATE", "STREET_NAME", "LATITUDE", "LONGITUDE")]
+speed <- speed[,c("CAMERA.ID", "LATITUDE", "LONGITUDE")]
+
+# Latitude and longitude is required 
 speed <- speed[!is.na(speed$LONGITUDE),]
 speed <- speed[!is.na(speed$LATITUDE),]
-
 crash <- crash[!is.na(crash$LONGITUDE),]
 crash <- crash[!is.na(crash$LATITUDE),]
 
-# Strip crash of uneeded features
-crash <- crash[,c("CRASH_DATE", "STREET_NAME", "LATITUDE", "LONGITUDE")]
+# Give a feature to both crash and speed called type
+crash$type <- "crash"
+speed$type <- "speed"
 
-# Remove observations with little variance
-speedRem <- nearZeroVar(speed)
-crashRem <- nearZeroVar(crash)
+# Aggregate df's by unique identifiers
+coords <- merge(crash, speed, by=c("LONGITUDE", "LATITUDE"), all=TRUE)
+coords <- coords[, c("type.x", "type.y", "LONGITUDE", "LATITUDE")]
+coords$type <- paste(coords$type.x, coords$type.y)
 
-# Apply NA to all blank entries in our data
-speed[speed == ""] <- NA
-crash[crash == ""] <- NA
+# Remove NA tags
+coords$type <- gsub("NA", "", as.character(coords$type))
+coords <- coords[,c("type", "LONGITUDE", "LATITUDE")]
 
-if(length(speedRem) > 0)
-  speed <- speed[-speedRem]
+# Finding Each Camera
+library(sqldf)
+everyCamera <- sqldf('SELECT DISTINCT * FROM speed')
+everyCamera$CAMERA.ID <- gsub("CHI", "", as.character(everyCamera$CAMERA.ID))
+everyCamera$type <- NULL
 
-if(length(crashRem) > 0)
-  crash <- crash[-crashRem]
+# Loop function to assign a nearest speeding camera
+# to every crash based on Euclidean distance. 
+this.distance <- 0
+chosen.camera <- NULL
+regions <- rep("blank", nrow(crash))
+cameras <- everyCamera[,"CAMERA.ID"]
+
+crash.long <- crash[,"LONGITUDE"]
+cam.long <- everyCamera[,"LONGITUDE"]
+cam.lat <- everyCamera[,"LATITUDE"]
+crash.lat <- crash[,"LATITUDE"]
+for(x in 1:nrow(crash)){
+  shortest.distance <- 10000
+  #Find shortest distance for given crash
+  for(y in 1:nrow(everyCamera)){
+    # Compute Euclidean distance
+    this.distance <- (crash.long[x] - cam.long[y])^2 + (crash.lat[x] - cam.lat[y])^2
+    if(this.distance < shortest.distance){
+      shortest.distance = this.distance
+      regions[x] <- cameras[y]
+    }
+  }
+}
+crash$regions <- regions
 
 # Write both updated speed and crash datasets to file
+# Also write aggregrated crash/speed 'coords' data to file
 write.csv(crash, file="../Data/Mod_Crash.csv")
 write.csv(speed, file="../Data/Mod_Speed.csv")
-
-# Combine datasets by location
-
-
+write.csv(coords, file="../Data/Aggr_Crash_Speed.csv")
