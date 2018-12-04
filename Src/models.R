c769339c268ce045c4288ed3c927eb857ff6be8f
Madhumita Krishnan <44059919+madhumitakrish@users.noreply.github.com>
2018-12-03 20:30:55 -0500

    Update models.R

--- Src/models.R
+++ Src/models.R
@@ -9,9 +9,10 @@ count.grid <- seq(from = min(everyCamera$ViolationCount), to = max(everyCamera$V
 newdata <-list(count.grid)
 pred=predict(fit,newdata,se=TRUE)
 se.bands <- cbind(pred$fit + 2*pred$se.fit, pred$fit - 2*pred$se.fit)
-plot(everyCamera$ViolationCount,everyCamera$CrashesCount,cex=.5,col="darkgray",main="Degree-4Polynomial")
-lines(count.grid,pred$fit)
-matlines(count.grid,se.bands,lwd=1,col="blue",lty=3)
+
+plot(everyCamera$ViolationCount,everyCamera$CrashesCount,cex=.5,col="black",main="Degree-4Polynomial",xlab="Speed Violation",ylab="Traffic Crash")
+lines(count.grid,pred$fit,lwd=2)
+matlines(count.grid,se.bands,lwd=1,col="blue",lty="dashed")
 
 
 
@@ -22,13 +23,13 @@ knot.position <- c(250,750,1450)
 fit <- lm(everyCamera$CrashesCount ~ bs(everyCamera$ViolationCount, knots=knot.position), data=everyCamera)
 count.grid <- seq(from = min(everyCamera$ViolationCount), to = max(everyCamera$ViolationCount),length.out = 150)
 pred <- predict(fit,newdata=list(count=count.grid) , se=TRUE)
-#png(filename="../Images/SplineCrashvsViolation.png")
+
 plot(everyCamera$ViolationCount,everyCamera$CrashesCount,xlab="Speed Violation",ylab="Traffic Crash", cex=.5, col="black",main="Cubic Spline Model")
 lines(count.grid, pred$fit, lwd=2, col="blue")
 lines(count.grid, pred$fit + 2*pred$se, lty="dashed",lwd=1, col="blue")
 lines(count.grid, pred$fit - 2*pred$se, lty="dashed",lwd=1, col="blue")
 abline(v = knot.position, lty="dashed")
-#dev.off()
+
 summary(fit)
 
 
@@ -41,6 +42,7 @@ plot(everyCamera$ViolationCount,everyCamera$CrashesCount, cex=.5, col="darkgray"
 lines(fit,  col="red", lwd=2)
 lines(fit2, col="blue", lwd=2)
 legend("topright", legend=c("10 DF"," (LOOCV)"), col=c("red", "blue"),lty=1, lwd=2, cex=.8)
+
 
 
 
